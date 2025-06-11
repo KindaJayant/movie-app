e41e79c25897c1ccb4fdf237d861236e406d9d79
KindaJayant <iamjayant246@gmail.com>
2025-06-11 11:06:22 +0530

    fix: icon fixes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,34 +1,35 @@
-import React from 'react';
+import React from 'react'
 
-const MovieCard = ({movie : {title, vote_average, poster_path, release_date, original_language }}) => {
-    return (
-        <div className = "movie-card">
-            <img src = {poster_path ? `https://image.tmdb.org/t/p/original/${poster_path}` : '/no-poster-v.png'}
-            alt = {title}/>
+const MovieCard = ({ movie:
+  { title, vote_average, poster_path, release_date, original_language }
+}) => {
+  return (
+    <div className="movie-card">
+      <img
+        src={poster_path ?
+          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+        alt={title}
+      />
 
-            <div className = "mt-4">
-                <h3>{title}</h3>
+      <div className="mt-4">
+        <h3>{title}</h3>
 
-                <div className = "content">
-                    <div className = "rating">
-                        <img src = "Rating.svg" alt = "Star Icon" />
-                        <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
-                    </div>
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+          </div>
 
-                    <span>•</span>
-                    <p className = "lang">{original_language}</p>
+          <span>•</span>
+          <p className="lang">{original_language}</p>
 
-                    <span>•</span>
-                    <p className = "year">
-                        {release_date ? release_date.split('-')[0] : 'N/A'}
-                    </p>
-
-
-                </div>
-            </div>
-
+          <span>•</span>
+          <p className="year">
+            {release_date ? release_date.split('-')[0] : 'N/A'}
+          </p>
         </div>
-    );
-};
-
-export default MovieCard;
+      </div>
+    </div>
+  )
+}
+export default MovieCard
